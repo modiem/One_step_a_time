fac9d4ea50a72c9d28324dec07f3c649b641f449
Mo Yang <mo_yang@foxmail.com>
2021-03-24 20:11:45 +0100

    python algorithms

---
+++ Python-Algorithms/algorithms.py
@@ -0,0 +1,19 @@
+from stack import Stack
+
+def convert_int_to_bin(dec_num):
+    
+    if dec_num == 0:
+        return 0
+    
+    s = Stack()
+
+    while dec_num > 0:
+        remainder = dec_num % 2
+        s.push(remainder)
+        dec_num = dec_num // 2
+
+    bin_num = ""
+    while not s.is_empty():
+        bin_num += str(s.pop())
+
+    return bin_num
